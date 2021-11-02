3f5405b6f39c835e871baaf50c4c97333cbf3a5f
Logan Caraco <lbc.dna@gmail.com>
2021-11-02 10:11:00 -0700

    proof of concept on func capture

--- debugger.py
+++ debugger.py
@@ -1,4 +1,4 @@
-from ast import stmt, unparse
+from ast import *
 
 class Debugger:
     def __init__(self):
@@ -6,6 +6,11 @@ class Debugger:
         self.user_names = dict()
 
     def run(self, statement: stmt):
+        if isinstance(statement, Expr) and \
+                isinstance(statement.value, Call) and \
+                    isinstance(statement.value.func, Name) and \
+                        statement.value.func.id in self.user_names:
+            print('called', statement.value.func.id)
         print('highlight line(s)', *range(statement.lineno, statement.end_lineno + 1))
         try:
             locals = dict()
