081512ff95ee53e79d4295a05938fef52f291708
Logan Caraco <lbc.dna@gmail.com>
2021-11-02 10:55:47 -0700

    debugger cleanup

--- debugger.py
+++ debugger.py
@@ -11,19 +11,17 @@ class Debugger:
     def user_call_wrapper(self, f, f_name):
         def inner(*args, **kwargs):
             bindings = ins.signature(f).bind(*args, **kwargs)
+            print('calling', f_name, bindings)
             for b in self.user_names[f_name].body:
                 self.run(b, bindings=bindings.arguments, record_locals=False)
         return inner
-    
+
     def run(self, statement: stmt, *, bindings=None, record_locals=True):
         print('highlight line(s)', *range(statement.lineno, statement.end_lineno + 1))
 
-        if isinstance(statement, Expr) and \
-                isinstance(statement.value, Call) and \
-                    isinstance(statement.value.func, Name) and \
-                        statement.value.func.id in self.user_names:
-            print('called', statement.value.func.id)
-            # do something here?
+        if self.is_user_defined_func_call(statement):
+            pass # do something here?
+        
         try:
             locals = bindings or dict()
             
@@ -42,3 +40,9 @@ class Debugger:
                 self.user_names[k] = statement
         except Exception as e:
             print(e)
+
+    def is_user_defined_func_call(self, statement: stmt):
+        return isinstance(statement, Expr) and \
+                isinstance(statement.value, Call) and \
+                    isinstance(statement.value.func, Name) and \
+                        statement.value.func.id in self.user_names
