49aa5f0b097bfcdc4ae4015c2c923be33e10ac29
Logan Caraco <lbc.dna@gmail.com>
2021-11-02 16:35:35 -0700

    working on cb return values

--- debugger.py
+++ debugger.py
@@ -1,4 +1,5 @@
 from ast import *
+from functools import partial
 from typing import *
 
 import inspect as ins
@@ -8,15 +9,22 @@ class Debugger:
         self.globals = dict()
         self.user_names = dict()
 
-    def user_call_wrapper(self, f, f_name):
+    def user_call_wrapper(self, f: Callable, f_name: str):
         def inner(*args, **kwargs):
             bindings = ins.signature(f).bind(*args, **kwargs)
-            print('calling', f_name, bindings)
+            print('debugging', f_name, bindings)
             for b in self.user_names[f_name].body:
-                self.run(b, bindings=bindings.arguments, record_locals=False)
+                v = self.run(b, bindings=bindings.arguments)
+                if isinstance(v, SyntaxError) and v.msg.index('return') >= 0:
+                    break
+                if isinstance(v, Exception):
+                    return v
+            
+            # todo: figure out how to execute f(*args, **kwargs) with ctxt self.globals
+            return None
         return inner
 
-    def run(self, statement: stmt, *, bindings=None, record_locals=True):
+    def run(self, statement: stmt, *, bindings=None):
         print('highlight line(s)', *range(statement.lineno, statement.end_lineno + 1))
 
         if self.is_user_defined_func_call(statement):
@@ -25,10 +33,10 @@ class Debugger:
         try:
             locals = bindings or dict()
             
-            exec(unparse(statement), self.globals, locals)
+            ret = exec(unparse(statement), self.globals, locals)
 
-            if not record_locals or not locals:
-                return
+            if not locals:
+                return ret
             
             if bindings:
                 for b in bindings.keys():
@@ -36,10 +44,14 @@ class Debugger:
             
             print(locals)
             for k, v in locals.items():
-                self.globals[k] = self.user_call_wrapper(v, k) if callable(v) else v
+                self.globals[k] = self.user_call_wrapper(v, k) \
+                    if callable(v) else v
                 self.user_names[k] = statement
+            
+            return ret
         except Exception as e:
-            print(e)
+            print('** exception', e)
+            return e
 
     def is_user_defined_func_call(self, statement: stmt):
         return isinstance(statement, Expr) and \
