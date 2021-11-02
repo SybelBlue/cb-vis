55c13edf81f4cad007e9e87d522a7d66d8bb11da
Logan Caraco <lbc.dna@gmail.com>
2021-11-02 10:08:16 -0700

    refactor

--- ast_mapper.py
+++ ast_mapper.py
@@ -1,18 +1,13 @@
-from typing import *
-
 from ast import *
 
-from dataclasses import dataclass
-
-from debugger import Debugger
-
-
-@dataclass
-class SourceFileMap:
-    pass
-
-
 class StatementExtractor(NodeVisitor):
+    @staticmethod
+    def extract_from_file(path) -> list[stmt]:
+        with open(path, 'r') as f:
+            ls = ''.join(f.readlines())
+
+        return StatementExtractor().visit(parse(ls))
+    
     def __init__(self):
         super().__init__()
         self.stmts = list()
@@ -25,22 +20,4 @@ class StatementExtractor(NodeVisitor):
                 self.visit(child)
         return self.stmts
 
-
-if __name__ == '__main__':
-    with open('test/dummy_editor.py', 'r') as f:
-        ls = ''.join(f.readlines())
-
-    code = parse(ls, 'editor.py')
-
-    top_level_statements = StatementExtractor().visit(code)
-
-    db = Debugger()
-
-    for s in top_level_statements:
-        # cmd = input('>')
-        # if not cmd:
-        #     db.run(s)
-        # elif cmd.lower() == 'q':
-        #     break
-        db.run(s)
 
---
+++ main.py
@@ -0,0 +1,16 @@
+from ast_mapper import StatementExtractor
+from debugger import Debugger
+
+
+if __name__ == '__main__':
+    top_level_statements = StatementExtractor.extract_from_file('test/dummy_editor.py')
+
+    db = Debugger()
+
+    for s in top_level_statements:
+        # cmd = input('>')
+        # if not cmd:
+        #     db.run(s)
+        # elif cmd.lower() == 'q':
+        #     break
+        db.run(s)
