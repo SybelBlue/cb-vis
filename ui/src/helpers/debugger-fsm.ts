667512d7e568a5a7507e84e9e60c76322d11c4e9
Logan Caraco <lbc.dna@gmail.com>
2021-11-16 19:55:31 -0800

    created the debugger fsm

---
+++ ui/src/helpers/debugger-fsm.ts
@@ -0,0 +1,74 @@
+import { createMachine, assign, AssignAction } from 'xstate';
+
+interface TraceData {
+  frame: { lineno: number; locals: Record<string, unknown> };
+  type: string;
+  arg?: string;
+  arg_name?: string;
+}
+
+interface Context {
+  index: number;
+  data: TraceData[];
+}
+
+type Event =
+  | { type: 'NEXT' }
+  | { type: 'PREV' }
+  | { type: 'STOP' }
+  | { type: 'LOAD'; payload: TraceData[] };
+
+const clampIndex = (n: number, array: unknown[]): number =>
+  Math.max(0, Math.min(array.length - 1, n));
+
+const delta = (d: number): AssignAction<Context, Event> =>
+  assign((context: Context) => ({
+    index: clampIndex(context.index + d, context.data),
+  }));
+
+const debuggerMachine = createMachine<Context, Event>(
+  {
+    context: { index: 0, data: [] },
+    initial: 'uninit',
+    states: {
+      uninit: {
+        on: {
+          LOAD: {
+            target: 'idle',
+            actions: (_ctxt, e) => ({
+              index: 0,
+              data: e.payload,
+            }),
+          },
+        },
+      },
+      idle: {
+        on: {
+          NEXT: {
+            actions: ['next', 'debug'],
+          },
+          PREV: {
+            actions: ['prev', 'debug'],
+          },
+          LOAD: {
+            actions: ['activate', 'debug'],
+          },
+          STOP: {
+            target: 'uninit',
+            actions: ['debug'],
+          },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      // eslint-disable-next-line no-console
+      debug: console.log,
+      next: delta(1),
+      prev: delta(-1),
+    },
+  }
+);
+
+export default debuggerMachine;
