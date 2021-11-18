bf7e27d538110a7a9b0aa7cbf66fc00186a32f83
Logan Caraco <lbc.dna@gmail.com>
2021-11-18 11:55:49 -0800

    added better types

--- ui/src/types/pyodide.ts
+++ ui/src/types/pyodide.ts
@@ -1,9 +1,24 @@
 export interface Pyodide {
   runPython: (source: string) => void;
+  globals: PyProxy;
+  runPythonAsync: (source: string) => Promise<void>;
   PythonError: {
     name: string;
   };
 }
+
+export interface PyProxy {
+  length: number;
+  type: string;
+  get: (name: string) => unknown | PyProxy;
+  [Symbol.iterator]: () => { next: () => { value: string } };
+}
+
+export type TraceExec = (
+  code: string,
+  reporter: (json: string) => void,
+  globals?: PyProxy
+) => PyProxy;
 
 export interface TraceData {
   frame: { lineno: number; locals: Record<string, unknown> };
