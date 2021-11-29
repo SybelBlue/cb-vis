3337b8ebffbd8ece88e30f3550459b685355c9ae
Logan Caraco <lbc.dna@gmail.com>
2021-11-29 13:18:42 -0800

    starting code changes

--- ui/src/atoms/editor-atoms.ts
+++ ui/src/atoms/editor-atoms.ts
@@ -1,19 +1,22 @@
 import { atom } from 'jotai';
 
 export const documentAtom = atom<string>(
-  `<h1>Hello cb-vis!</h1>\n<button>Click Me!</button>\n<ol id='list'></ol>`
+  `<h1>Hello cb-vis!</h1>\n<button>Click Me!</button>\n<button>Or Me!</button>\n<ol id='list'></ol>`
 );
 export const iframeAtom = atom<string>('');
 export const programAtom = atom<string>(
   `def pows_of_2(n):
-  for i in range(n):
-    print(2 ** i)
+    for i in range(n):
+        print(2 ** i)
 
 pows_of_2(3)
 
 set_callback('button', 'click', "add_item('howdy')")
 
+i = 0
 def add_item(text):
-	append_to('#list', '<li> ' + text + ' </li>')
+    global i
+    i += 1
+    append_to('#list', f'<li> {text} {i} </li>')
 `
 );
